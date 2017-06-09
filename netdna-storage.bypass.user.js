8a6dbc8492b1f88971d170b53fa0c8d41018b7e8
RoeeM <roee@mail.com>
2017-06-09 18:50:11 +0300

    Cross browser support

--- netdna-storage.bypass.user.js
+++ netdna-storage.bypass.user.js
@@ -3,19 +3,31 @@
 // @namespace     https://github.com/JonnyShuali/netdna-storage-click-bypass
 // @description   Bypass the clicking on netdna-storage.
 // @updateURL     https://github.com/JonnyShuali/netdna-storage-click-bypass/raw/master/netdna-storage.bypass.user.js
-// @version       3
+// @version       4
 // @include       http://www.netdna-storage.com/f/*
 // @include       http://www.netdna-storage.com/step/*
 // @include       http://netdna-storage.com/f/*
 // @include       http://netdna-storage.com/step/*
 // @run-at        document-start
+// @grant         GM_xmlhttpRequest 
 // ==/UserScript==
 
-window.addEventListener('beforescriptexecute', function() {
-    var g = document.getElementsByClassName('plan-footer-item');
-    for (var i = 0; i < g.length; i++) {
-        if (g[i].getAttribute('data-href')) {
-            window.location = g[i].getAttribute('data-href');
+
+
+GM_xmlhttpRequest({
+    method: 'GET',
+    url: window.location.href,
+    onload: function(response) {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(response.responseText, 'text/html');
+        if (doc === null) {
+            return;
+        }
+        var g = doc.getElementsByClassName('plan-footer-item');
+        for (var i = 0; i < g.length; i++) {
+            if (g[i].getAttribute('data-href')) {
+                location.assign(g[i].getAttribute('data-href'));
+            }
         }
     }
-}, false);
+});
