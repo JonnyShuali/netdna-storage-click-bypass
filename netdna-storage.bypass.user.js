ca3e28a2ab1f67870d54f6d6e009f0720388b583
RoeeM <roee@mail.com>
2017-06-15 22:04:08 +0300

    new version, fix netdna problem

--- netdna-storage.bypass.user.js
+++ netdna-storage.bypass.user.js
@@ -3,11 +3,15 @@
 // @namespace     https://github.com/JonnyShuali/netdna-storage-click-bypass
 // @description   Bypass the clicking on netdna-storage.
 // @updateURL     https://github.com/JonnyShuali/netdna-storage-click-bypass/raw/master/netdna-storage.bypass.user.js
-// @version       4
+// @version       5
 // @include       http://www.netdna-storage.com/f/*
 // @include       http://www.netdna-storage.com/step/*
 // @include       http://netdna-storage.com/f/*
 // @include       http://netdna-storage.com/step/*
+// @include       https://www.netdna-storage.com/f/*
+// @include       https://www.netdna-storage.com/step/*
+// @include       https://netdna-storage.com/f/*
+// @include       https://netdna-storage.com/step/*
 // @run-at        document-start
 // @grant         GM_xmlhttpRequest 
 // ==/UserScript==
@@ -25,8 +29,8 @@ GM_xmlhttpRequest({
         }
         var g = doc.getElementsByClassName('plan-footer-item');
         for (var i = 0; i < g.length; i++) {
-            if (g[i].getAttribute('data-href')) {
-                location.assign(g[i].getAttribute('data-href'));
+            if (g[i].getAttribute('data-link')) {
+                location.assign(g[i].getAttribute('data-link'));
             }
         }
     }
