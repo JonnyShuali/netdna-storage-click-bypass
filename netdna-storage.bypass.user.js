79c61055e192b334051adadb41a207b08e54da55
JonnyShuali <roee@mail.com>
2017-11-16 22:22:43 +0200

    bugfix due new netdna-storage version

--- netdna-storage.bypass.user.js
+++ netdna-storage.bypass.user.js
@@ -3,7 +3,7 @@
 // @namespace     https://github.com/JonnyShuali/netdna-storage-click-bypass
 // @description   Bypass the clicking on netdna-storage.
 // @updateURL     https://github.com/JonnyShuali/netdna-storage-click-bypass/raw/master/netdna-storage.bypass.user.js
-// @version       5
+// @version       6
 // @include       http://www.netdna-storage.com/f/*
 // @include       http://www.netdna-storage.com/step/*
 // @include       http://netdna-storage.com/f/*
@@ -29,8 +29,8 @@ GM_xmlhttpRequest({
         }
         var g = doc.getElementsByClassName('plan-footer-item');
         for (var i = 0; i < g.length; i++) {
-            if (g[i].getAttribute('data-link')) {
-                location.assign(g[i].getAttribute('data-link'));
+            if (g[i].getAttribute('href')) {
+                location.assign(g[i].getAttribute('href'));
             }
         }
     }
